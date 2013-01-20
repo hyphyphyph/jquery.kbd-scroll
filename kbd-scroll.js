df8aee5d96d4747bd044915017e93d67d28325b7
Derek McTavish Mounce <d@lvst.co>
2013-01-20 15:21:47 -0500

    Take into account device rotation. Refactor as a proper jQuery plugin.

--- kbd-scroll.js
+++ kbd-scroll.js
@@ -1,50 +1,69 @@
-$(function () {
-  var htmlHeight = $('html').height();
-  var keyboardActive = false;
+(function ($) {
+  $.fn.kbdScroll = function () {
+    var $root = $(this);
 
-  var setActive = function () {
-    $('.kbd-scroll').each(function () {
-      var $kbdScroll = $(this);
-      var $overscroll = $(this).children('.overscroll');
+    var htmlHeight = $('html').height();
+    var htmlWidth = $('html').width();
 
-      var height = $overscroll.children('.content').height();
-
-      $overscroll.height(height);
-      // $kbdScroll.scrollTop(height);
-      $kbdScroll.scrollTop(0);
-    });
-  };
-
-  var setInactive = function () {
-    $('.kbd-scroll').each(function () {
-      var $kbdScroll = $(this);
-      var $overscroll = $(this).children('.overscroll');
-
-      var height = $overscroll.children('.content').height();
-
-      $overscroll.height($kbdScroll.height());
-      $kbdScroll.scrollTop($kbdScroll.height());
-    });
-  };
-
-  setTimeout(function () {
     // Detect keyboard based on window size changes
-    // TODO: take width into account
     $(window).on('resize', function () {
-      var newHeight = $('html').height();
+      // Take into account rotation
+      var newWidth = $('html').width();
+      if (newWidth === htmlWidth) {
+        var newHeight = $('html').height();
 
-      if (newHeight < htmlHeight) {
-        keyboardActive = true;
-        setActive();
+        if (newHeight < htmlHeight) {
+          setActive();
+        }
+        else if (newHeight > htmlHeight) {
+          setInactive();
+        }
+
+        htmlHeight = newHeight;
       }
-      else if (newHeight > htmlHeight) {
-        keyboardActive = false;
+      else {
         setInactive();
       }
-
-      htmlHeight = newHeight;
+      htmlWidth = newWidth;
     });
 
-    $('.kbd-scroll').scrollTop(10000);
-  }, 1);
-});
+    var setActive = function () {
+      $('.kbd-scroll').each(function () {
+        var $kbdScroll = $root;
+        var $overscroll = $(this).children('.overscroll');
+
+        var height = $overscroll.children('.content').height();
+
+        $overscroll.height(height);
+        $kbdScroll.scrollTop(0);
+      });
+    };
+
+    var setInactive = function () {
+      $('.kbd-scroll').each(function () {
+        var $kbdScroll = $root;
+        var $overscroll = $kbdScroll.children('.overscroll');
+
+        var height = $overscroll.children('.content').height();
+
+        // If the content is actually taller than the page,
+        // it should scroll. This is particularly relevant
+        // in landscape mode.
+        if (height > $kbdScroll.height()) {
+          $overscroll.height(height);
+          $kbdScroll.scrollTop(0);
+        }
+        else {
+          $overscroll.height($kbdScroll.height());
+          $kbdScroll.scrollTop($kbdScroll.height());
+        }
+      });
+    };
+
+    // Hack to circumvent the Android initial page size bug.
+    setTimeout(function () {
+      $root.scrollTop(10000);
+      $root.children('.overscroll').css('height', '100%');
+    }, 1);
+  };
+})(jQuery);
